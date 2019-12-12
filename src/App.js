53e0ce4bf150cb48dda7bee70f8ff19dffb13989
Edwin <deronviledwin@gmail.com>
2019-12-12 17:55:00 -0500

    mvp is done currently working on a stretch of a random date gen-button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
+import Appcontent from './Header';
 
 function App() {
+
+  const [apiReq, setApiReq] = useState([]);
+  const dayRandom = Math.round(Math.random()*28);
+  const years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017];
+  const yearRandom = years[Math.floor(Math.random()* years.length)];
+  const monthRandom = Math.round(Math.random()*12);
+  useEffect(() =>{
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=EO0h1b6xsNy1K10ItVFw6jS5f2XY8SDfPyXaWKoD&${yearRandom}-${monthRandom}-11`
+      )
+      .then(response =>{
+        console.log(response.data);
+        setApiReq(response.data);
+      })
+      .catch(error =>{
+        console.log("the data was not read", error);
+      });
+  }, []);
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Appcontent
+        title={apiReq.title}
+        explanation={apiReq.explanation}
+        image={apiReq.url}
+        date={apiReq.date}
+      />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const AppContent = props =>{
+
+    const style = {
+        'position': 'fixed',
+        'top': 0, 
+        'left': 0,
+        'minWidth': '100%',
+        'minHeight': '100%',
+        'zIndex': '-1',
+    }
+
+    return (
+        <div className="nasa-constainer" key={props.id}>
+
+            <div className="hero" style={style}>
+                <img src = {props.image} style= {style} alt={props.title}/>
+            </div>
+
+            <div className='top-content'>
+                <h1>Random Space!</h1>
+                <p>Explore space and time!</p>
+            </div>
+
+            <div className="middle-content">
+                <h1>{props.title}</h1>
+                    <h2>{props.date}</h2>   
+            </div>
+
+            <div className="lower-content">
+                <p>{props.explanation}</p>
+            </div>
+                      
+        </div>
+    );
+}
+
+export default AppContent;
